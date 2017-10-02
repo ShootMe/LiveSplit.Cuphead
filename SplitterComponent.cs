15262f68e048ef6e0c0665c0c37d91995b040d46
ShootMe <birreller@gmail.com>
2017-10-02 17:15:07 -0600

    Removed duplicate autosplitters

--- SplitterComponent.cs
+++ SplitterComponent.cs
@@ -1,6 +1,7 @@
 ﻿#if !Info
 using LiveSplit.Model;
 using LiveSplit.UI;
+using LiveSplit.UI.Components;
 #endif
 using System;
 using System.Collections.Generic;
@@ -229,6 +230,19 @@ namespace LiveSplit.Cuphead {
 
 #if !Info
 		public void Update(IInvalidator invalidator, LiveSplitState lvstate, float width, float height, LayoutMode mode) {
+			//Remove duplicate autosplitter componenets
+			IList<ILayoutComponent> components = lvstate.Layout.LayoutComponents;
+			bool hasAutosplitter = false;
+			for (int i = components.Count - 1; i >= 0; i--) {
+				ILayoutComponent component = components[i];
+				if (component.Component is SplitterComponent) {
+					if ((invalidator == null && width == 0 && height == 0) || hasAutosplitter) {
+						components.Remove(component);
+					}
+					hasAutosplitter = true;
+				}
+			}
+
 			GetValues();
 		}
 
